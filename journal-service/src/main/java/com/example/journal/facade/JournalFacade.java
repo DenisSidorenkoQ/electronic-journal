0191c7abb52ff7ce488e710622ae9c010a10b2d6
Denis <apricot103@mail.ru>
2023-01-24 16:34:11 +0300

    add save and get methods

--- journal-service/src/main/java/com/example/journal/facade/JournalFacade.java
+++ journal-service/src/main/java/com/example/journal/facade/JournalFacade.java
@@ -2,6 +2,7 @@ package com.example.journal.facade;
 
 import com.example.journal.model.Journal;
 import com.example.journal.service.JournalService;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,5 +15,13 @@ public class JournalFacade {
     public Journal saveOrGet(final Journal journal) {
         return journalService.getByGroupId(journal.getGroupId())
                 .orElseGet(() -> journalService.save(journal));
+    }
+
+    public Optional<Journal> getById(final Long journalId) {
+        return journalService.getById(journalId);
+    }
+
+    public Optional<Journal> getByGroupId(final Long groupId) {
+        return journalService.getByGroupId(groupId);
     }
 }
