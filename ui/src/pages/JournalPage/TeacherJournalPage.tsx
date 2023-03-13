11ba16f56855a33b5d905195cca725ee3fbde131
Denis <apricot103@mail.ru>
2023-03-13 20:04:57 +0300

    divided page elements into components

--- ui/src/pages/JournalPage/TeacherJournalPage.tsx
+++ ui/src/pages/JournalPage/TeacherJournalPage.tsx
@@ -1,20 +1,10 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {
-    SelectChangeEvent,
-    FormControl,
-    InputLabel,
-    MenuItem,
-    Select,
-    IconButton,
-    useMediaQuery, Dialog, DialogTitle, DialogContent, DialogActions
-} from "@mui/material";
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import {useSessionStore} from "../../store";
 import SubjectService from "../../service/SubjectService";
 import {GroupHasSubject, Subject} from "../../model/SubjectState";
-import {useEffect, useReducer, useRef} from "react";
+import {useEffect} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './Journal.css';
@@ -22,35 +12,12 @@ import TeacherService from "../../service/TeacherService";
 import {Group} from "../../model/GroupState";
 import GroupService from "../../service/GroupService";
 import subjectService from "../../service/SubjectService";
-import Table from "react-bootstrap/Table";
-import {Mark} from "../../model/MarkState";
-import {StudyPass} from "../../model/StudyPassState";
-import lessonService from "../../service/LessonService";
-import {Lesson} from "../../model/LessonState";
-import studentService from "../../service/StudentService";
-import markService from "../../service/MarkService";
-import studyPassService from "../../service/StudyPassService";
-import {Student} from "../../model/StudentState";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import journalService from "../../service/JournalService";
-import {Journal} from "../../model/JournalState";
-import passService from "../../service/PassService";
+import SubjectSelect from "../../components/Journal/select/SubjectSelect";
+import GroupSelect from "../../components/Journal/select/GroupSelect";
+import TeacherDataGrid from "../../components/Journal/data-grid/TeacherDataGrid";
+import AddLessonDialog from "../../components/Journal/dialog/AddLessonDialog";
 
 const theme = createTheme();
-
-function getDateFromTimestamp (timestamp: number ) {
-    return new Date(timestamp * 1000).toLocaleDateString('ru-RU',
-        { year: '2-digit', month: '2-digit', day: '2-digit'});
-}
-
-function studyPassConverter ( pass: boolean ) {
-    if (pass) {
-        return "н";
-    } else {
-        return "";
-    }
-}
 
 const TeacherJournalPage = () => {
     const [teacherId, setTeacherId] = React.useState(0);
@@ -60,15 +27,6 @@ const TeacherJournalPage = () => {
     const [groupIdSet, setGroupIdSet] = React.useState(() => new Set());
     const [groupList, setGroupList] = React.useState<Group[]>([]);
     const [subjectList, setSubjectList] = React.useState<Subject[]>([]);
-    const [lessonList, setLessonList] = React.useState<Lesson[]>([]);
-    const [studentList, setStudentList] = React.useState<Student[]>([]);
-    const [markList, setMarkList] = React.useState<Mark[]>([]);
-    const [passList, setPassList] = React.useState<StudyPass[]>([]);
-    const [journal, setJournal] = React.useState<Journal>();
-    const [lessonTitle, setLessonTitle] = React.useState("");
-
-    const [open, setOpen] = React.useState(false);
-    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 
     let user = useSessionStore(state => state.user);
 
@@ -104,265 +62,14 @@ const TeacherJournalPage = () => {
         subjectService.getSubjectsByTeacherIdAndGroupId(teacherId, selectedGroupId).then(subjects => setSubjectList(subjects));
     }, [selectedGroupId]);
 
-    useEffect(() => {
-        lessonService.getAllLessonsByGroupIdAndSubjectId(selectedGroupId, selectedSubjectId).then(lessons => setLessonList(lessons));
-    }, [selectedSubjectId, journal]);
-
-    useEffect(() => {
-        studentService.getStudentsByGroupId(selectedGroupId).then(students => setStudentList(students));
-
-        markService.getMarkBySubjectIdAndGroupId(selectedSubjectId, selectedGroupId).then(marks => {
-            setMarkList(marks);
-        });
-
-        studyPassService.getStudyPassBySubjectIdAndGroupId(selectedSubjectId, selectedGroupId).then(pass => {
-            setPassList(pass);
-        })
-    }, [lessonList]);
-
-    const handleGroupChange = (event: SelectChangeEvent) => {
-        const groupId = parseInt(event.target.value);
-
-        setSelectedGroupId(groupId);
-    };
-
-    const handleSubjectChange = (event: SelectChangeEvent) => {
-        const subjectId = parseInt(event.target.value);
-
-        setSelectedSubjectId(subjectId);
-    };
-
-    const handleOpenAddLessonDialogButton = () => {
-        setOpen(true);
-    };
-
-    const handleCloseAddLessonDialog = () => {
-        setOpen(false);
-    };
-
-    const handleChangeMark = (event: any) => {
-        const number: number = event.target.textContent;
-        const studentId: number = event.currentTarget.dataset.studentid;
-        const lessonId: number = event.currentTarget.dataset.lessonid;
-
-        if (number > 0 && number < 11 ) {
-            markService.upsertMark(lessonId, studentId, number);
-        } else {
-            alert("The number must be greater than 0 and less than 10");
-        }
-    };
-
-    const handleChangePass = (event: any) => {
-        const pass: string = event.target.textContent;
-        const studentId: number = event.currentTarget.dataset.studentid;
-        const lessonId: number = event.currentTarget.dataset.lessonid;
-
-        if(pass === "Н" || pass === "н") {
-            passService.upsertPass(lessonId, studentId, true);
-        } else if (pass === "" || pass === " ") {
-            passService.upsertPass(lessonId, studentId, false);
-        } else {
-            alert("Н - student did not come to lesson\nX - student came to lesson");
-        }
-    };
-
-    const handleSaveLessonButton = async () => {
-        if (lessonTitle === "") return;
-
-        const currentTimestamp = Math.floor(Date.now() / 1000);
-        const journalBuffer = await journalService.getJournalByGroupId(selectedGroupId);
-        setJournal(journalBuffer);
-        await lessonService.saveLesson(journalBuffer.id, selectedSubjectId, lessonTitle, currentTimestamp);
-        setOpen(false);
-    };
-
-    const handleLessonTitleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-        setLessonTitle(event.target.value);
-    }
-
-    function subjectSelect() {
-        if (subjectList.length === 0) return;
-        return (
-            <div>
-                <FormControl sx={{m: 1, minWidth: 80}}>
-                    <InputLabel id="Subjects">Subjects</InputLabel>
-                    <Select
-                        labelId="Subjects"
-                        id="Subjects"
-                        value={selectedSubjectId.toString()}
-                        onChange={handleSubjectChange}
-                        label="Subjects"
-                        sx={{width: 300}}
-                    >
-                        {
-                            subjectList.map((subject) => (
-                                <MenuItem key={subject.id} value={subject.id}>{subject.name}</MenuItem>
-                            ))
-                        }
-                    </Select>
-                </FormControl>
-            </div>
-        )
-    }
-
-    function addLessonDialog() {
-        return (
-            <>
-                <IconButton aria-label="add new lesson" size="large" onClick={handleOpenAddLessonDialogButton}>
-                    <AddCircleOutlineIcon fontSize="inherit"/>
-                </IconButton>
-                <div>
-                    <Dialog
-                        fullScreen={fullScreen}
-                        open={open}
-                        onClose={handleCloseAddLessonDialog}
-                        aria-labelledby="dialog-title"
-                    >
-                        <DialogTitle id="dialog-title">
-                            {"Creating a new lesson"}
-                        </DialogTitle>
-                        <DialogContent>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="lessonTitle"
-                                label="lesson title"
-                                name="lessonTitle"
-                                value={lessonTitle}
-                                onChange={handleLessonTitleChange}/>
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={handleSaveLessonButton}>
-                                Save
-                            </Button>
-                            <Button onClick={handleCloseAddLessonDialog} autoFocus>
-                                Cancel
-                            </Button>
-                        </DialogActions>
-                    </Dialog>
-                </div>
-            </>
-        )
-    }
-
-    function grid() {
-        if (lessonList.length === 0) return;
-        return (
-            <>
-                {addLessonDialog()}
-                <Table className="table w-auto text-xsmall" bordered size="sm">
-                    <thead>
-                    <tr>
-                        <th rowSpan={2} colSpan={1}>
-                            Students
-                        </th>
-                        {lessonList.map(lesson => {
-                            return (
-                                <th rowSpan={1} colSpan={2}>
-                                    {lesson.themeName + "\t" + getDateFromTimestamp(lesson.dateTimestamp)}
-                                </th>
-                            );
-                        })}
-                    </tr>
-                    <tr>
-                        {lessonList.map(lesson => {
-                            return (
-                                <>
-                                    <th>Mark</th>
-                                    <th>Pass</th>
-                                </>
-                            );
-                        })}
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {studentList.map(student => {
-                        return (
-                            <tr>
-                                <td>{student.fio}</td>
-                                {lessonList.map(lesson => {
-                                    let lessonMark: Mark | undefined;
-                                    let lessonPass: StudyPass | undefined;
-
-                                    lessonMark = markList.find(mark => {
-                                        return mark.studentId === student.id && mark.lessonId === lesson.id;
-                                    });
-
-                                    lessonPass = passList.find(pass => {
-                                        return pass.studentId === student.id && pass.lessonId === lesson.id;
-                                    });
-
-                                    if (lessonMark !== undefined && lessonPass !== undefined) {
-                                        return (
-                                            <React.Fragment>
-                                                <td id={lessonMark.id.toString()} data-studentId={student.id} data-lessonId={lesson.id} contentEditable onBlur={handleChangeMark}>
-                                                    {lessonMark.number}
-                                                </td>
-                                                <td id={lessonPass.id.toString()} data-studentId={student.id} data-lessonId={lesson.id} contentEditable onBlur={handleChangePass}>{studyPassConverter(lessonPass.pass)}</td>
-                                            </React.Fragment>
-                                        );
-                                    }
-                                    if (lessonMark !== undefined && lessonPass === undefined) {
-                                        return (
-                                            <React.Fragment>
-                                                <td id={lessonMark.id.toString()} data-studentId={student.id} data-lessonId={lesson.id} contentEditable onBlur={handleChangeMark}>{lessonMark.number}</td>
-                                                <td data-studentId={student.id} data-lessonId={lesson.id} contentEditable onBlur={handleChangePass}></td>
-                                            </React.Fragment>
-                                        );
-                                    }
-                                    if (lessonPass !== undefined && lessonMark === undefined) {
-                                        return (
-                                            <React.Fragment>
-                                                <td data-studentId={student.id} data-lessonId={lesson.id} contentEditable onBlur={handleChangeMark}/>
-                                                <td id={lessonPass.id.toString()} data-studentId={student.id} data-lessonId={lesson.id} contentEditable onBlur={handleChangePass}>
-                                                    {studyPassConverter(lessonPass.pass)}
-                                                </td>
-                                            </React.Fragment>
-                                        );
-                                    }
-                                    if (lessonPass === undefined && lessonMark === undefined) {
-                                        return (
-                                            <React.Fragment>
-                                                <td data-studentId={student.id} data-lessonId={lesson.id} contentEditable onBlur={handleChangeMark}/>
-                                                <td data-studentId={student.id} data-lessonId={lesson.id} contentEditable onBlur={handleChangePass}></td>
-                                            </React.Fragment>
-                                        );
-                                    }
-                                })}
-                            </tr>
-                        );
-                    })}
-                    </tbody>
-                </Table></>
-        )
-    }
-
     return (
         <ThemeProvider theme={theme}>
             <Container component="main" maxWidth="xs">
-                <div>
-                    <FormControl sx={{m: 1, minWidth: 80}}>
-                        <InputLabel id="Groups">Groups</InputLabel>
-                        <Select
-                            labelId="Groups"
-                            id="Groups"
-                            value={selectedGroupId.toString()}
-                            onChange={handleGroupChange}
-                            label="Groups"
-                            sx={{width: 300}}
-                        >
-                            {
-                                groupList.map((group) => (
-                                    <MenuItem key={group.id} value={group.id}>{group.name}</MenuItem>
-                                ))
-                            }
-                        </Select>
-                    </FormControl>
-                </div>
-                {subjectSelect()}
+                {GroupSelect(groupList, selectedGroupId, setSelectedGroupId)}
+                {SubjectSelect(subjectList, selectedSubjectId, setSelectedSubjectId)}
             </Container>
-            {grid()}
+            {AddLessonDialog(theme, selectedGroupId, selectedSubjectId)}
+            {TeacherDataGrid(selectedGroupId, selectedSubjectId)}
         </ThemeProvider>
     );
 };
